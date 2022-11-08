f1c68b4907e0e8a37fb5b9d7da89109443993acc
KauhareniaNastassia <kavgorenya_a@tut.by>
2022-11-08 23:59:35 +0300

    add components

---
+++ src/common/c2-SuperButton/SuperButton.tsx
@@ -0,0 +1,27 @@
+import React, {ButtonHTMLAttributes, DetailedHTMLProps} from 'react'
+import css from './SuperButton.module.css'
+
+// тип пропсов обычной кнопки, children в котором храниться название кнопки там уже описан
+type DefaultButtonPropsType = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>
+
+type SuperButtonPropsType = DefaultButtonPropsType & {
+    red?: boolean
+}
+
+const SuperButton: React.FC<SuperButtonPropsType> = (
+    {
+        red, className,
+        ...restProps// все остальные пропсы попадут в объект restProps, там же будет children
+    }
+) => {
+    const finalClassName = `${css.button} ${red ? css.red : css.default} ${className}`
+
+    return (
+        <button
+            className={finalClassName}
+            {...restProps} // отдаём кнопке остальные пропсы если они есть (children там внутри)
+        />
+    )
+}
+
+export default SuperButton
