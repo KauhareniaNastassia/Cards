ae8b260dec2b5be5bf08e739d5dd2b3d20c8aee0
nazarPtyt <ockap90@libero.it>
2022-11-14 09:56:13 +0100

    feat: add Formik & validation on PasswordRecovery page

--- src/components/PasswordRecovery/PasswordRecovery.tsx
+++ src/components/PasswordRecovery/PasswordRecovery.tsx
@@ -2,18 +2,36 @@ import React from 'react'
 
 import { Button } from '@mui/material'
 import TextField from '@mui/material/TextField/TextField'
+import { useFormik } from 'formik'
 
+import { validateUtil } from '../../utils/validate'
 import s from '../SignUp/SignUp.module.css'
 
 export const PasswordRecovery = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+    },
+    validate: validateUtil,
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2))
+    },
+  })
+
   return (
     <section className={s.wrapp}>
       <div className={s.inner}>
         <h2>Forgot your password?</h2>
-        <form className={s.formWrapp}>
-          <TextField name="email" label="Email" placeholder={'Email'} />
+        <form className={s.formWrapp} onSubmit={formik.handleSubmit}>
+          <TextField
+            label="Email"
+            placeholder={'Email'}
+            {...formik.getFieldProps('email')}
+            error={!!(formik.touched.email && formik.errors.email)}
+            helperText={formik.errors.email}
+          />
           <span>Enter your email address and we will send you further instructions </span>
-          <Button variant="contained" style={{ borderRadius: '20px' }}>
+          <Button type="submit" variant="contained" style={{ borderRadius: '20px' }}>
             Send Instructions
           </Button>
           <Button
