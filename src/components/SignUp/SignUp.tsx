b68bfea7f5c874c4094a08e8627a543985372c4c
nazarPtyt <ockap90@libero.it>
2022-11-14 13:18:24 +0100

    feat: add Formik & validation on SingUp + CreateNewPassword pages

--- src/components/SignUp/SignUp.tsx
+++ src/components/SignUp/SignUp.tsx
@@ -2,27 +2,61 @@ import React from 'react'
 
 import { Button } from '@mui/material'
 import TextField from '@mui/material/TextField/TextField'
+import { useFormik } from 'formik'
+import { Link } from 'react-router-dom'
 
+import { PATH } from '../../app/App'
 import InputPassword from '../../common/inputsFromMateUI/InputPassword'
+import { validateUtil } from '../../utils/validate'
 
 import s from './SignUp.module.css'
 
 const SignUp = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+      confirmPassword: '',
+    },
+    validate: validateUtil,
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2))
+    },
+  })
+
   return (
     <section className={s.wrapp}>
       <div className={s.inner}>
         <h2>Sign Up</h2>
-        <form className={s.formWrapp}>
-          <TextField name="email" label="Email" placeholder={'Email'} />
-          <InputPassword nameField={'Password'} />
-          <InputPassword nameField={'Confirm Password'} />
-          <Button variant="contained" style={{ borderRadius: '20px' }}>
+        <form className={s.formWrapp} onSubmit={formik.handleSubmit}>
+          <TextField
+            label="Email"
+            placeholder={'Email'}
+            {...formik.getFieldProps('email')}
+            error={!!(formik.touched.email && formik.errors.email)}
+            helperText={formik.errors.email}
+          />
+          <InputPassword
+            nameField={'Password'}
+            {...formik.getFieldProps('password')}
+            error={!!(formik.touched.password && formik.errors.password)}
+          />
+          {formik.touched.password && formik.errors.password ? (
+            <div style={{ color: 'red' }}>{formik.errors.password}</div>
+          ) : null}
+          <InputPassword
+            nameField={'Confirm Password'}
+            {...formik.getFieldProps('confirmPassword')}
+            error={!!(formik.touched.confirmPassword && formik.errors.confirmPassword)}
+          />
+          {formik.touched.confirmPassword && formik.errors.confirmPassword ? (
+            <div style={{ color: 'red' }}>{formik.errors.confirmPassword}</div>
+          ) : null}
+          <Button type="submit" variant="contained" style={{ borderRadius: '20px' }}>
             Sign Up
           </Button>
           <span>Already have an account?</span>
-          <Button variant="text" style={{ color: 'blue' }}>
-            Sign In
-          </Button>
+          <Link to={PATH.Login}>SIGN IN</Link>
         </form>
       </div>
     </section>
--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -2,21 +2,32 @@ type validateType = {
   email?: string
   name?: string
   password?: string
+  confirmPassword?: string
 }
 
 export const validateUtil = (values: validateType) => {
   const errors: validateType = {}
 
-  if (!values.email) {
-    errors.email = 'Required'
-  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-    errors.email = 'Invalid email address'
+  if (values.email !== undefined) {
+    if (!values.email) {
+      errors.email = 'Required'
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+      errors.email = 'Invalid email address'
+    }
   }
-
-  if (!values.password) {
-    errors.password = 'Required'
-  } else if (values.password.length < 4) {
-    errors.password = 'Password Must be 4 characters or more'
+  if (values.password !== undefined) {
+    if (!values.password) {
+      errors.password = 'Required'
+    } else if (values.password.length < 4) {
+      errors.password = 'Password Must be 4 characters or more'
+    }
+  }
+  if (values.confirmPassword !== undefined) {
+    if (!values.confirmPassword) {
+      errors.confirmPassword = 'Required'
+    } else if (values.confirmPassword !== values.password) {
+      errors.confirmPassword = 'Password is not the same !!!'
+    }
   }
 
   // if (!values.firstName) {
