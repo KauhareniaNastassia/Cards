851031d1a606d44399e42fd6d706523f00c38aba
nazarPtyt <ockap90@libero.it>
2022-11-14 10:57:58 +0100

    fix: problem with validation and show error message of password input

--- src/components/CreateNewPassword/CreateNewPassword.tsx
+++ src/components/CreateNewPassword/CreateNewPassword.tsx
@@ -1,26 +1,44 @@
 import React from 'react'
 
 import { Button } from '@mui/material'
+import { useFormik } from 'formik'
 
 import InputPassword from '../../common/inputsFromMateUI/InputPassword'
+import { validateUtil } from '../../utils/validate'
 import s from '../SignUp/SignUp.module.css'
 
 export const CreateNewPassword = () => {
+  const formik = useFormik({
+    initialValues: {
+      password: '',
+    },
+    validate: validateUtil,
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2))
+    },
+  })
+
   return (
     <section className={s.wrapp}>
       <div className={s.inner}>
         <h2>Create New Password</h2>
-        <form className={s.formWrapp}>
+        <form className={s.formWrapp} onSubmit={formik.handleSubmit}>
           <InputPassword
             nameField={'Password'}
-            onChangeText={value => {
-              console.log(value)
-            }}
+            {...formik.getFieldProps('password')}
+            error={!!(formik.touched.password && formik.errors.password)}
           />
+          {formik.touched.password && formik.errors.password ? (
+            <div style={{ color: 'red' }}>{formik.errors.password}</div>
+          ) : null}
           <span style={{ opacity: '50%' }}>
             Create new password and we will send you further instructions to email
           </span>
-          <Button variant="contained" style={{ borderRadius: '20px', textTransform: 'inherit' }}>
+          <Button
+            type="submit"
+            variant="contained"
+            style={{ borderRadius: '20px', textTransform: 'inherit' }}
+          >
             Create new password
           </Button>
         </form>
--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -12,16 +12,17 @@ export const validateUtil = (values: validateType) => {
   } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
     errors.email = 'Invalid email address'
   }
+
+  if (!values.password) {
+    errors.password = 'Required'
+  } else if (values.password.length < 4) {
+    errors.password = 'Password Must be 4 characters or more'
+  }
+
   // if (!values.firstName) {
   //   errors.firstName = 'Required'
   // } else if (values.firstName.length > 15) {
   //   errors.firstName = 'Must be 15 characters or less'
-  // }
-
-  // if (!values.lastName) {
-  //   errors.lastName = 'Required'
-  // } else if (values.lastName.length > 20) {
-  //   errors.lastName = 'Must be 20 characters or less'
   // }
 
   return errors
