15a9ab1d7225797f8da1a13bd5443fa53da96fd3
nazarPtyt <ockap90@libero.it>
2022-12-14 23:33:14 +0100

    feat(ToggleThemeButton): add custom button to change theme

--- src/common/ToggleThemeButton/ToggleThemeButton.tsx
+++ src/common/ToggleThemeButton/ToggleThemeButton.tsx
@@ -1,20 +1,40 @@
 import { SetAppThemeAC } from '../../redux/app-reducer'
 import { saveState } from '../../redux/localStorage.'
-import { store } from '../../redux/store'
 import { useAppDispatch, useAppSelector } from '../../utils/hooks'
+
+import s from './ToggleThemeButton.module.css'
 
 export const ToggleThemeButton = () => {
   const theme = useAppSelector(state => state.app.theme)
   const dispatch = useAppDispatch()
 
   const HandleChangeTheme = () => {
-    dispatch(SetAppThemeAC(theme === 'light' ? 'dark' : 'light'))
-    saveState(theme === 'light' ? 'dark' : 'light')
+    if (theme === 'dark') {
+      dispatch(SetAppThemeAC('light'))
+      saveState('light')
+    } else {
+      dispatch(SetAppThemeAC('dark'))
+      saveState('dark')
+    }
   }
 
   return (
     <div>
-      <button onClick={HandleChangeTheme}>theme</button>
+      <input id={s.toggle_checkbox} type="checkbox" checked={theme === 'light'} />
+      <label onClick={HandleChangeTheme} className={s.label} htmlFor="toggle_checkbox">
+        <div id={s.star}>
+          <div className={s.star} id={s.star1}>
+            ★
+          </div>
+          <div className={s.star} id={s.star2}>
+            ★
+          </div>
+        </div>
+        <div id={s.moon}></div>
+      </label>
     </div>
   )
 }
+
+// 12. Sun And Moon Toggle Switch
+// https://alvarotrigo.com/blog/toggle-switch-css/
